5db599b8b3f988714b8b113747aa147f4c8aa417
Lorendos <leonid.osman.p@gmail.com>
2026-02-05 19:30:17 +0100

    Actually , Actually added the functionality

---
+++ src/parseJson.ts
@@ -0,0 +1,30 @@
+/*
+REXSchool Copyright (C) 2026 Lorendos aka Leonid
+Full license agrement is under the file LICENSE
+Uses code from AdriDevelopsThings/xschool-node on github
+*/
+export const parseJSON = (json: any) => {
+  const terms: any[] = []
+  for (const term of json.Terms) {
+    const outTerm: any = { id: term.Id, title: term.Title, current: term.IsCurrent }
+    const students = json.Students.filter((s: any) => s.TermId == term.Id)
+    outTerm['students'] = students.map((student: any) => ({
+      id: student.Id,
+      firstname: student.Firstname,
+      lastname: student.Lastname,
+      subjects: student.Subjects.map((subject: any) => ({
+        id: subject.Id,
+        title: subject.Title,
+        marks: subject.Ratings.map((rating: any) => ({
+          id: rating.ColumnId,
+          title: rating.Title,
+          dateOfCorrection: rating.DateOfCorrection,
+          dateOfRating: rating.DateOfRating,
+          mark: rating.MarkValue ? +rating.MarkValue : null,
+        })),
+      })),
+    }))
+    terms.push(outTerm)
+  }
+  return terms
+}
